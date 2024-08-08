a98fc289f1a1c33b185f7c1fc9b3ae655b3e1c95
Ronenii <ronen.glm@gmail.com>
2024-08-08 13:56:08 +0300

    - removed redundant table - Changed primary key in group memberships - adjusted documentation - removed indexes

--- init.sql
+++ init.sql
@@ -13,7 +13,7 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE user_roles (
-    group_id UUID NOT NULL,
+    group_id VARCHAR(255) NOT NULL,
     user_id UUID NOT NULL,
     role_id UUID REFERENCES roles(role_id) ON DELETE CASCADE,
     PRIMARY KEY (group_id, user_id),
@@ -22,14 +22,13 @@ CREATE TABLE user_roles (
 
 -- Group Management Service Schema
 CREATE TABLE groups (
-    group_id UUID PRIMARY KEY,
-    group_name VARCHAR(255)
+    group_id VARCHAR(255) PRIMARY KEY,
+    group_name VARCHAR(255),
 );
 
 CREATE TABLE group_memberships (
-    membership_id UUID PRIMARY KEY,
     user_id UUID REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
-    group_id UUID REFERENCES groups(group_id) ON DELETE CASCADE NOT NULL
+    group_id VARCHAR(255) REFERENCES groups(group_id) ON DELETE CASCADE NOT NULL PRIMARY KEY (group_id, user_id),
 );
 
 -- Poll Management Service Schema
@@ -37,44 +36,26 @@ CREATE TABLE polls (
     poll_id UUID PRIMARY KEY,
     title VARCHAR(255),
     description TEXT,
-    num_answers_allowed INT,
-    creator_id UUID REFERENCES users(user_id) ON DELETE SET NULL NOT NULL,
-    group_id UUID REFERENCES groups(group_id) ON DELETE SET NULL NOT NULL,
+    nof_answers_allowed INT,
+    creator_id UUID,
+    group_id VARCHAR(255),
     time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     time_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deadline TIMESTAMP
+    deadline TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE answer_options (
-    answer_id UUID PRIMARY KEY,
-    poll_id UUID REFERENCES polls(poll_id) ON DELETE CASCADE,
+CREATE TABLE voting_item_options (
+    voting_item_id SERIAL PRIMARY KEY,
+    poll_id UUID REFERENCES polls (poll_id),
     ordinal INT,
-    answer_text TEXT,
-    num_of_votes INT
-);
-
-CREATE TABLE voting_deadlines (
-    deadline_id SERIAL PRIMARY KEY,
-    poll_id UUID REFERENCES polls(poll_id) ON DELETE CASCADE NOT NULL,
-    deadline_datetime TIMESTAMP
+    description TEXT,
+    vote_count INT
 );
 
 -- Voting Service Schema
 CREATE TABLE votes (
     vote_id UUID PRIMARY KEY,
     user_id UUID REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
-    answer_id UUID REFERENCES answer_options(answer_id) ON DELETE CASCADE NOT NULL,
+    voting_item_id UUID REFERENCES voting_item_options(voting_item_id) ON DELETE CASCADE NOT NULL,
     vote_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- Indexes for Foreign Keys
-CREATE INDEX idx_user_roles_user_id ON user_roles(user_id);
-CREATE INDEX idx_user_roles_role_id ON user_roles(role_id);
-CREATE INDEX idx_group_memberships_user_id ON group_memberships(user_id);
-CREATE INDEX idx_group_memberships_group_id ON group_memberships(group_id);
-CREATE INDEX idx_polls_creator_id ON polls(creator_id);
-CREATE INDEX idx_polls_group_id ON polls(group_id);
-CREATE INDEX idx_answer_options_poll_id ON answer_options(poll_id);
-CREATE INDEX idx_voting_deadlines_poll_id ON voting_deadlines(poll_id);
-CREATE INDEX idx_votes_user_id ON votes(user_id);
-CREATE INDEX idx_votes_answer_id ON votes(answer_id);
